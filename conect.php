f0db97d69fbc31b1a15fa886e80134c07c5ff8c8
YogaAP_SY <Yoga_metalz@yahoo.com>
2016-05-11 16:51:10 -0700

    initial commit

--- conect.php
+++ conect.php
@@ -1,4 +1,4 @@
 <?php
 	$koneksi = mysqli_connect("ap-cdbr-azure-east-c.cloudapp.net
-", "b33285f173fbca","844a8ca6" , "ilkomdeveloper") or die(mysql_error());
+", "b33285f173fbca","844a8ca6" , "ilkomdeveloper");
 ?>
