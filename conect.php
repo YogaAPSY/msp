6ac8e3f13d2d2b981ad1032eb40ec7737d7c813d
YogaAP_SY <Yoga_metalz@yahoo.com>
2016-05-11 06:43:40 -0700

    asdasdas

--- conect.php
+++ conect.php
@@ -1,4 +1,4 @@
 <?php
 	$koneksi = mysqli_connect("ap-cdbr-azure-east-c.cloudapp.net
-", "b33285f173fbca","844a8ca6" , "ilkomdeveloper");
+", "b33285f173fbca","844a8ca6" , "ilkomdeveloper") or die(mysql_error());
 ?>
